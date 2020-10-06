911eaa0a8511679d38eec4b8c04643cb89cc03ce
MarchalKobe <55789963+MarchalKobe@users.noreply.github.com>
2020-10-06 12:22:49 +0200

    Update

--- script/app.js
+++ script/app.js
@@ -1,13 +1,43 @@
-function handleFloatingLabel() {
+let email = {},
+    password = {},
+    signInButton;
+
+const handleFloatingLabel = () => {
    
 }
 
-function handlePasswordSwitcher() {
-    
-}
+const handlePasswordSwitcher = () => {
+    const passwordInput = document.querySelector(".js-password-input"),
+    passwordToggle = document.querySelector(".js-password-toggle");
+
+    if(!passwordInput || !passwordToggle) {
+        console.error("De classes werden niet gevonden", passwordInput, passwordToggle);
+        return;
+    };
+
+    passwordToggle.addEventListener("change", () => {
+        if(passwordInput.type == "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    });
+};
+
+const getDOMElements = () => {
+    email.input = document.querySelector("");
+    email.field = document.querySelector("");
+    email.errorMessage = document.querySelector("");
+
+    password.input = document.querySelector("");
+    password.field = document.querySelector("");
+    password.errorMessage = document.querySelector("");
+
+    signInButton = document.querySelector(".js-sign-in-button");
+};
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
-    handleFloatingLabel();
+    // handleFloatingLabel();
     handlePasswordSwitcher();
 });
